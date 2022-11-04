148884edc062037a22ae9df8396c76eb3463f176
Romain Blary <romain.blary.uni@gmail.com>
2022-11-04 11:31:55 +0100

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/styles/[name][extname]",
+        },
+      },
+    },
+  },
+});
